28fd4c70aa16c3e5353a9b4d24acd0a4a71ab089
Eric Yeh <e122426@gmail.com>
2020-12-08 23:27:01 +0800

    update requirements

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.models import (
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('9rvIouCQrgODVyuKEfdDdFbaxPffXrTs9qyYuugiJoqHjITy/lcCPls0Xabr8XFzDtN2wVgNkHLmEr4RjgYLcnQjukZzDENd6U8y4gJYgcdLyNPL0nn2x24SABPLmApQ7keULkePNWQZf2AR3QG4oQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('b3dd7bebba7dd30ab06e25add81eb054')
+handler = WebhookHandler('b3dd7bebba7dd30ab06e25add81eb054')  #line_bot_api跟handler，就是接line-bot裡面的參數
 
 
 @app.route("/callback", methods=['POST']) #有人用瀏覽器發送訊號到我的網址，我再執行下面的程式
@@ -32,14 +32,32 @@ def callback():
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
-    return 'OK'
+    return 'OK'  #return之上的程式碼就不用動~
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage) #就我所知我們的動作就是寫在handler之下
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #event.message.text是使用者傳來的訊息，前面這行程式碼是回傳使用者傳來的訊息
+
+    
+    if '草泥馬訓練紀錄' in event.message.text:  #-----
+        
+    try:
+        record_list = prepare_record(event.message.text) #這一段我還在測試中 是要用來寫入資料庫的><
+        reply = line_insert_record(record_list)
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply)
+        )
+            
+    except:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='失敗了')   #-----
+        )
 
 
 if __name__ == "__main__":
---
+++ read_database.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a 你-APP-的名字').read()[:-1]
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
+
+cursor.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'alpaca_training'")
+
+data = []
+while True:
+    temp = cursor.fetchone()
+    if temp:
+        data.append(temp)
+    else:
+        break
+print(data)
