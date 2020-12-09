88eba21e9fbd88168e7beea69552201e77f2c791
Eric Yeh <e122426@gmail.com>
2020-12-10 01:59:00 +0800

    keep trying

--- phoetalk.py
+++ phoetalk.py
@@ -13,28 +13,28 @@ import utils, call_database
 line_bot_api = LineBotApi('9rvIouCQrgODVyuKEfdDdFbaxPffXrTs9qyYuugiJoqHjITy/lcCPls0Xabr8XFzDtN2wVgNkHLmEr4RjgYLcnQjukZzDENd6U8y4gJYgcdLyNPL0nn2x24SABPLmApQ7keULkePNWQZf2AR3QG4oQdB04t89/1O/w1cDnyilFU=')
 
 
-def insert_record(event):
+# def insert_record(event):
     
-    if '草泥馬訓練紀錄' in event.message.text:
+#     if '草泥馬訓練紀錄' in event.message.text:
         
-        try:
-            record_list = utils.prepare_record(event.message.text)
-            reply = call_database.line_insert_record(record_list)
+#         try:
+#             record_list = utils.prepare_record(event.message.text)
+#             reply = call_database.line_insert_record(record_list)
 
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply)
-            )
+#             line_bot_api.reply_message(
+#                 event.reply_token,
+#                 TextSendMessage(text=reply)
+#             )
 
-        except:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='失敗了')
-            )
+#         except:
+#             line_bot_api.reply_message(
+#                 event.reply_token,
+#                 TextSendMessage(text='失敗了')
+#             )
 
-        return True
-    else:
-        return False
+#         return True
+#     else:
+#         return False
 
 
 def insert_record2(event):
