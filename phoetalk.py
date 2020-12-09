eea60fc12bdfb427dab2316b543070767d420322
Eric Yeh <e122426@gmail.com>
2020-12-10 01:32:04 +0800

    keep trying

--- phoetalk.py
+++ phoetalk.py
@@ -42,13 +42,13 @@ def insert_record2(event):
     if '租屋' in event.message.text:
         
         try:
-        record_list = utils.prepare_record2(event.message.text)
-        reply = call_database.line_insert_record2(record_list2)
+            record_list = utils.prepare_record2(event.message.text)
+            reply = call_database.line_insert_record2(record_list2)
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply)
-            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply)
+                )
 
         except:
             line_bot_api.reply_message(
