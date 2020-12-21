24728643593e112c701913a85188566017158270
Eric Yeh <e122426@gmail.com>
2020-12-22 02:13:00 +0800

    button

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageCarouselTemplate
 )
 
 import utils, call_database, phoetalk
