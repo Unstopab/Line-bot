8078d559975ce5b76b0791cf7e08cd988f6aac85
Eric Yeh <e122426@gmail.com>
2020-12-22 23:46:17 +0800

    button

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('9rvIouCQrgODVyuKEfdDdFbaxPffXrTs9qyYuugiJoqHjITy/lcCPls0Xabr8XFzDtN2wVgNkHLmEr4RjgYLcnQjukZzDENd6U8y4gJYgcdLyNPL0nn2x24SABPLmApQ7keULkePNWQZf2AR3QG4oQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('b3dd7bebba7dd30ab06e25add81eb054')  #line_bot_api跟handler，就是接line-bot裡面的參數
+parser = WebhookParser('b3dd7bebba7dd30ab06e25add81eb054') #button test 
+
 
 @app.route("/callback", methods=['POST']) #有人用瀏覽器發送訊號到我的網址，我再執行下面的程式
 def callback():
